76d922293ec768b11e250109cc967f990c0e6e9a
Hispano <jesus.diaz.gutierrez@gmail.com>
2013-04-13 15:44:37 +0200

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HuancavelicaEs::Application.config.secret_token = '96d95582c2204b92fcf7acba9cac8f4477016ae21999fc239c9805d18aadcc19f290c9484cd4e4b2681af7e57d46b8399a9b08bd079cdae9530592e46c6f5a21'
